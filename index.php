a8cbf5986b26607656ffcbb1067db5dd5177d805
prasht63 <prasht63@gmail.com>
2014-03-10 13:06:45 +0530

    Update index.php
    Markers as Circles with radius as magnitude parameter and colour as depth parameter.
    Markers show complete info about a earthquake when cursor is placed upon them

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <?php if(!isset($_REQUEST['date'])){$request="2015-03-06";/*some future date so that play back doesnt start on its own*/$persec=2;$mag=6;}
 else {$request=$_REQUEST['date'];$persec=$_REQUEST['persec'];$enddate=$_REQUEST['enddate'];$mag=$_REQUEST['mag'];}?>
@@ -32,35 +31,56 @@ var iterator = 0;
         document.getElementsByTagName('head')[0].appendChild(script);
       }
 
-     
-    var onMarkerClick = function() {
-      var marker = this;
-      var latLng = marker.getPosition();
-      infoWindow.setContent('<h3>Earthquake position at is:</h3>' +
-          latLng.lat() + ', ' + latLng.lng());
-
-      infoWindow.open(map, marker);
-    };
-     
+    
       window.eqfeed_callback = function(results) {
-        for (var i = 0; i < results.features.length; i++) {
-          setTimeout(function() {
+	  
+        for (var i = 0; i < results.features.length; i++) /*The results are stored in array results and can be accesed via the counter*/
+		{
+          setTimeout(function()
+		  {
       addMarker();
     }, i*<?php echo 1000/$persec?>);
-        }
+        } //adding marker fns
 		 function addMarker() {
-		 var coords = results.features[iterator].geometry.coordinates;
-          var latLng = new google.maps.LatLng(coords[1],coords[0]);
-          var marker = new google.maps.Marker({
+		 var earthquake = results.features[iterator];
+         var coords = earthquake.geometry.coordinates;
+         var latLng = new google.maps.LatLng(coords[1],coords[0]);
+         var marker = new google.maps.Marker({
             position: latLng,
-            map: map
-			
+            map: map,
+			title:""+iterator,
+			icon: getCircle(earthquake.properties.mag,results.features[iterator].geometry.coordinates[2]) /*To plot the marker as a circle with radius depending on magnitude and colour depending upon depth
+			*/
           }); 
-		  iterator++;google.maps.event.addListener(marker, 'click', onMarkerClick);document.getElementById("p1").innerHTML=results.features[iterator].properties.place;document.getElementById("p2").innerHTML=results.features[iterator].properties.mag;
-		  var date = new Date(results.features[iterator].properties.time);document.getElementById("p3").innerHTML=date;
+		  iterator++;
+		google.maps.event.addListener(marker, 'mouseover', function()/*Adding a eventlistener to add mouseover property*/
+		{
+	  
+      var marker = this;
+       var date = new Date(results.features[marker.title].properties.time);
+      infoWindow.setContent('<h3>Earthquake Details</h3><hr>Place:' +results.features[marker.title].properties.place+
+	  '<br>Magnitude:'+results.features[marker.title].properties.mag+'<br>Depth:'+results.features[marker.title].geometry.coordinates[2]+'km<br>Date:'+date);/*Writing information in the content window at the call time rather than run time.Increased efficiency and lower use of resources.*/
+
+      infoWindow.open(map, marker);/*CallingFunction to pop up information window*/
+    }); /*Printing information about the current earthquake being plotted*/
+		  document.getElementById("p1").innerHTML=results.features[iterator].properties.place;
+		  document.getElementById("p2").innerHTML=results.features[iterator].properties.mag;
+		  var date = new Date(results.features[iterator].properties.time);
+		  document.getElementById("p3").innerHTML=date;
 	  
 }
       }
+	  function getCircle(magnitude,depth) {var col;if(depth<70){col="red"}if(depth>=70 && depth <=300){col="yellow";}if(depth>300){col="green";} /*Setting colour of circle marker basedepth*/  
+	  return {
+  
+    path: google.maps.SymbolPath.CIRCLE,
+    fillColor:''+col,
+    fillOpacity: .4,
+    scale: Math.pow(2, magnitude/1.2) / Math.PI,//radius based on magnitude
+    strokeColor: 'blue',
+    strokeWeight: .5
+  };
+}
 	  var infoWindow = new google.maps.InfoWindow;
 
 
@@ -80,7 +100,7 @@ var iterator = 0;
          <p class="submit">
         <input type="submit" value="Play" />
       </p></form>
-      </p><font size=2>Current Earthquake Details:<p id="p1"></p><p id="p2"></p><p id="p3"></p></font></div>
+      </p><font size=2><!--Current Earthquake Details:<p id="p1"></p><p id="p2"></p><p id="p3"></p></font><p>-->Depth Code:<br> Red:<70kms <br>Yellow:70kms to 300 km</br>Green:>300kms</div>
     <div id="map_canvas"></div>
   </body>
 </html>
