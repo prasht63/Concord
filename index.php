1ea6fa75198fa9de37dd9337607685a9d2af9f9d
prasht63 <prasht63@gmail.com>
2014-03-13 01:22:06 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,33 +1,32 @@
 <!DOCTYPE html>
-<?php if(!isset($_REQUEST['date'])){$request="2015-03-06";/*some future date so that play back doesnt start on its own*/$persec=2;$mag=6;}
-else {$request=$_REQUEST['date'];$persec=$_REQUEST['persec'];$enddate=$_REQUEST['enddate'];$mag=$_REQUEST['mag'];}?>
-
 <html>
-  <head><link rel="stylesheet" href="style.css" type="text/css" media="all" />
+<head>
+    <link rel="stylesheet" href="style.css" type="text/css" media="all" />
     <style>
       html, body, #map_canvas { margin: 40; padding: 0; height: 100%; }
     </style>
-    <script
-      src="https://maps.googleapis.com/maps/api/js?sensor=false&libraries=visualization">
-      src="earthquake.js">
-    </script>
-   
-  </head>
-  <body onload="initialize()">
-  <div id="form-div"><font size=3 color="black">Concord Seismic Eruption Map</font>
-    <form class="form" id="form1" action="index.php">
-      <p class="name"><label for="name">Start Date </label>
-        <input name="date" type="text" placeholder="yyyy-mm-dd" class="" id="date"  />
-         <label for="name">End Date</label>
-		<input name="enddate" type="text" placeholder="yyyy-mm-dd" class="" id="enddate"  />
-       <label for="name">Earthquakes/sec </label>
-		<input name="persec" type="text" placeholder="Earthquake per second" class="" id="persec"  />
-		<label for="name">Magnitude</label>
-		<input name="mag" type="text" placeholder="Magnitude Cutoff" class="" id="mag"  />
-         <p class="submit">
-        <input type="submit" value="Play" />
-      </p></form>
-      </p><font size=2><!--Current Earthquake Details:<p id="p1"></p><p id="p2"></p><p id="p3"></p></font><p>-->Depth Code:<br> Red:<70kms <br>Yellow:70kms to 300 km</br>Green:>300kms</div>
-    <div id="map_canvas"></div>
-  </body>
+    <script src="https://maps.googleapis.com/maps/api/js?sensor=false&libraries=visualization"></script>
+	<script src="plotearthquake.js"></script>
+</head>
+  <body >
+  <div id="form-div">
+  <font size=3 color="black">Concord Seismic Eruption Map</font>
+  <br>
+  <label for="name">Start Date </label>
+  <input type="text" placeholder="yyyy-mm-dd" id="date"  />
+  <label for="name">End Date</label>
+  <input type="text" placeholder="yyyy-mm-dd" id="enddate"  />
+  <label for="name">Earthquakes/sec </label>
+  <input type="text" placeholder="Earthquake per second" id="persec"  />
+  <label for="name" value="6">Magnitude</label>
+  <input  type="text" placeholder="Magnitude Cutoff"  id="mag"  />
+  <p class="submit">
+  <input type="submit" onclick="initialize()" value="Play" />
+  </p>
+ 
+      </p><font size=2>Current Earthquake Details:<p id="p1"></p><!--</p><p id="p2"></p><p id="p3"></p></font><p>-->
+	  Depth Code:<br> Red:<70kms <br>Yellow:70kms to 300 km</br>Green:>300kms
+	  <input type="checkbox" id="tectonics" checked="checked" onclick="check()" />Tectonic Plates</div>
+      <div id="map_canvas"></div>
+   </body>
 </html>
